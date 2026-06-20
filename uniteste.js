1410aaabf3bcf9b9a84622fab93852f385d7bc46
Ângelo Nuffer <angelonuffer@gmail.com>
2026-06-20 15:29:55 -0300

    refactor: restructure testar function for improved clarity and error handling

--- uniteste.js
+++ uniteste.js
@@ -1,15 +1,12 @@
 import { bloco } from "./texto.js"
 import iguais from "./testar/iguais.js"
 
-export const testar = testes => testes.reduce(
-  ({ código, saída }, { função, argumento, retorno_esperado }, atual) => {
-    if (código !== 0) return { código, saída }
+export const testar = testes => {
+  const falhas = testes.map(({ função, argumento, retorno_esperado }) => {
     try {
-      const resultado = função(argumento)
-      if (! iguais(resultado, retorno_esperado)) {
-        return {
-          código: 1,
-          saída: bloco(`
+      const retorno = função(argumento)
+      if (! iguais(retorno, retorno_esperado)) {
+        return bloco(`
           . função:
           .   ${função.name || "anônima"}
           . argumento:
@@ -17,24 +14,31 @@ export const testar = testes => testes.reduce(
           . retorno esperado:
           .   ${JSON.stringify(retorno_esperado, null, 2).replace(/\n/g, "\n  ")}
           . retorno:
-          .   ${JSON.stringify(resultado, null, 2).replace(/\n/g, "\n  ")}
-        `) + `\n\n🚨 Teste ${atual + 1}/${testes.length} falhou!`,
-        }
+          .   ${JSON.stringify(retorno, null, 2).replace(/\n/g, "\n  ")}
+        `)
       }
     } catch (erro) {
-      return {
-        código: 1,
-        saída: bloco(`
-          . função:
-          .   ${função.name || "anônima"}
-          . argumento:
-          .   ${JSON.stringify(argumento, null, 2).replace(/\n/g, "\n  ")}
-          . erro interno:
-          .   ${erro.stack}
-        `) + `\n\n🚨 Teste ${atual + 1}/${testes.length} falhou!`,
-      }
+      return bloco(`
+        . função:
+        .   ${função.name || "anônima"}
+        . argumento:
+        .   ${JSON.stringify(argumento, null, 2).replace(/\n/g, "\n  ")}
+        . erro interno:
+        .   ${erro.stack}
+      `)
     }
-    return { código: 0, saída: `✅ Todos os ${testes.length} testes passaram!` }
-  },
-  { código: 0, saída: "" }
-)
+    return null
+  }).filter(resultado => resultado !== null)
+  if (falhas.length === 0) return {
+    código: 0,
+    saída: `✅ Todos os ${testes.length} testes passaram!`,
+  }
+  if (falhas.length === 1) return {
+    código: 1,
+    saída: `${falhas[0]}\n\n🚨 1 de ${testes.length} teste falhou.`,
+  }
+  return {
+    código: 1,
+    saída: `${falhas.join("\n\n")}\n\n🚨 ${falhas.length} de ${testes.length} testes falharam.`,
+  }
+}
