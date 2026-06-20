6603279df7631c0cebb3e4d398a58dff0f5956c3
Ângelo Nuffer <angelonuffer@gmail.com>
2026-06-20 13:17:55 -0300

    refactor: simplify testing structure in testar function for improved clarity

--- uniteste.js
+++ uniteste.js
@@ -1,42 +1,39 @@
 import { bloco } from "./texto.js"
 import iguais from "./testar/iguais.js"
 
-export const testar = grupos_testes => {
-  const total = grupos_testes.reduce((acc, { testes }) => acc + testes.length, 0)
+export const testar = testes => {
+  const total = testes.length
   let atual = 0
-
-  for (const { função, testes } of grupos_testes) {
-    for (const { argumento, retorno_esperado } of testes) {
-      atual++
-      try {
-        const resultado = função(argumento)
-        if (! iguais(resultado, retorno_esperado)) {
-          return {
-            código: 1,
-            saída: bloco(`
-            . função:
-            .   ${função.name || "anônima"}
-            . argumento:
-            .   ${JSON.stringify(argumento, null, 2).replace(/\n/g, "\n  ")}
-            . retorno esperado:
-            .   ${JSON.stringify(retorno_esperado, null, 2).replace(/\n/g, "\n  ")}
-            . retorno:
-            .   ${JSON.stringify(resultado, null, 2).replace(/\n/g, "\n  ")}
-          `) + `\n\n🚨 Teste ${atual}/${total} falhou!`,
-          }
-        }
-      } catch (erro) {
+  for (const { função, argumento, retorno_esperado } of testes) {
+    atual++
+    try {
+      const resultado = função(argumento)
+      if (! iguais(resultado, retorno_esperado)) {
         return {
           código: 1,
           saída: bloco(`
-            . função:
-            .   ${função.name || "anônima"}
-            . argumento:
-            .   ${JSON.stringify(argumento, null, 2).replace(/\n/g, "\n  ")}
-            . erro interno:
-            .   ${erro.stack}
-          `) + `\n\n🚨 Teste ${atual}/${total} falhou!`,
+          . função:
+          .   ${função.name || "anônima"}
+          . argumento:
+          .   ${JSON.stringify(argumento, null, 2).replace(/\n/g, "\n  ")}
+          . retorno esperado:
+          .   ${JSON.stringify(retorno_esperado, null, 2).replace(/\n/g, "\n  ")}
+          . retorno:
+          .   ${JSON.stringify(resultado, null, 2).replace(/\n/g, "\n  ")}
+        `) + `\n\n🚨 Teste ${atual}/${total} falhou!`,
         }
+      }
+    } catch (erro) {
+      return {
+        código: 1,
+        saída: bloco(`
+          . função:
+          .   ${função.name || "anônima"}
+          . argumento:
+          .   ${JSON.stringify(argumento, null, 2).replace(/\n/g, "\n  ")}
+          . erro interno:
+          .   ${erro.stack}
+        `) + `\n\n🚨 Teste ${atual}/${total} falhou!`,
       }
     }
   }
